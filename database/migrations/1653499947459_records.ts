3c8d9ff9c9b886cf9214332a182e2e7ef9d677c8
vitor.indio <vitor.indio@owse.com.br>
2022-05-25 15:29:02 -0300

    criação migration records

---
+++ database/migrations/1653499947459_records.ts
@@ -0,0 +1,21 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Records extends BaseSchema {
+  protected tableName = 'records'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('record_id')
+      table.integer('user_id').unsigned().references('id').inTable('users').onUpdate('CASCADE')
+      table.timestamp('start_time').notNullable()
+      table.timestamp('end_time').notNullable()
+      table.string('project').notNullable
+      table.string('notes', 4000).nullable()
+      table.boolean('payed').defaultTo(false).nullable()
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
