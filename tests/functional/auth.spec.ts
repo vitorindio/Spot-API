ad2f93f5bec3c6f68bd12f12cdb5fd038cfcadb6
Arnon <arnonrdp@gmail.com>
2022-05-29 14:15:07 -0300

    Testes funcionais de autenticação

---
+++ tests/functional/auth.spec.ts
@@ -0,0 +1,48 @@
+import Database from '@ioc:Adonis/Lucid/Database'
+import { test } from '@japa/runner'
+import { UserFactory } from 'Database/factories'
+import faker from '@faker-js/faker'
+
+test.group('auth', (group) => {
+  group.each.setup(async () => {
+    await Database.beginGlobalTransaction()
+    return () => Database.rollbackGlobalTransaction()
+  })
+
+  test('[store] - able to authenticate with valid credentials', async ({ assert, client }) => {
+    const user = await UserFactory.merge({ password: 'secret' }).create()
+
+    const { response } = await client.post('/auth').json({
+      email: user.email,
+      password: 'secret'
+    })
+
+    assert.equal(response.status, 200)
+    assert.exists(response.body.token)
+  })
+
+  test('[store] - unable to authenticate with invalid credentials', async ({ assert, client }) => {
+    const { response } = await client.post('/auth').json({
+      email: faker.internet.email(),
+      password: 'secret'
+    })
+
+    assert.equal(response.status, 401)
+    assert.notExists(response.body.token)
+  })
+
+  test('[destroy] - able to delete token after logout', async ({ assert, client }) => {
+    const user = await UserFactory.merge({ password: 'secret' }).create()
+
+    const { response } = await client.post('/auth').json({
+      email: user.email,
+      password: 'secret'
+    })
+
+    await client.delete('/auth').header('Authorization', `Bearer ${response.body.token}`)
+
+    const token = await Database.from('api_tokens').where('user_id', user.id).first()
+
+    assert.isNull(token)
+  })
+})
