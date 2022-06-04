d6fd9eff5e843b1b64e87ad5011e5f0e06de650f
Arnon <arnonrdp@gmail.com>
2022-06-04 16:17:52 -0300

    Definido retorno de token com tratativa padrão para erros

--- app/Controllers/Http/Auth/Main.ts
+++ app/Controllers/Http/Auth/Main.ts
@@ -2,15 +2,12 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { StoreValidator } from 'App/Validators/Auth'
 
 export default class AuthController {
-  public async store({ request, response, auth }: HttpContextContract) {
+  public async store({ request, auth }: HttpContextContract) {
     const { email, password } = await request.validate(StoreValidator)
 
-    try {
-      const token = await auth.attempt(email, password, { expiresIn: '30 days' })
-      return token
-    } catch {
-      return response.unauthorized('Invalid credentials')
-    }
+    const token = await auth.attempt(email, password, { expiresIn: '30 days' })
+
+    return token
   }
 
   public async destroy({ auth }: HttpContextContract) {
