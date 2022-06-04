df7c05a6760a57e1403dcc073d71b969436f75c0
Arnon <arnonrdp@gmail.com>
2022-06-04 16:22:08 -0300

    Middleware de autenticação para `/admin/register`

--- start/routes/users.ts
+++ start/routes/users.ts
@@ -1,7 +1,7 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 // TODO: Adicionar middleware: apenas administrador pode registrar usuários
-Route.post('/admin/register', 'Users/Register.store')
+Route.post('/admin/register', 'Users/Register.store').middleware('auth')
 Route.get('/admin/register/:key', 'Users/Register.show')
 Route.put('/admin/register', 'Users/Register.update')
 
