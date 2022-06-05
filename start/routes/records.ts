56386ce91fa07dcc84ab8b1ec5d7758432fad967
Arnon <arnonrdp@gmail.com>
2022-06-04 23:54:49 -0300

    Exigência de autenticação para as rotas

--- start/routes/records.ts
+++ start/routes/records.ts
@@ -1,4 +1,8 @@
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/records', 'Records/Main.show')
-Route.post('/records', 'Records/Main.store')
+Route.get('/records', 'Records/Main.show').middleware('auth')
+Route.post('/records', 'Records/Main.store').middleware('auth')
+Route.put('/records', 'Records/Main.update').middleware('auth')
+Route.delete('/records', 'Records/Main.destroy').middleware('auth')
+
+Route.get('/admin/records', 'Records/Main.index').middleware('auth')
