46b7bd71195b3b7a19ef0d2b7806a1b7b4b626db
Arnon <arnonrdp@gmail.com>
2022-06-04 23:57:33 -0300

    Adicionados métodos `show`, `update` e `destroy`.

--- app/Controllers/Http/Records/Main.ts
+++ app/Controllers/Http/Records/Main.ts
@@ -1,24 +1,65 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { Records } from 'App/Models'
-import { StoreValidator } from 'App/Validators/Records'
+import { StoreValidator, UpdateValidator } from 'App/Validators/Records'
 
 export default class RecordsController {
-  public async store({ auth, request, response }: HttpContextContract) {
+  public async index({}: HttpContextContract) {
+    return Records.all()
+  }
+
+  public async store({ request, response, auth }: HttpContextContract) {
     const data = await request.validate(StoreValidator)
 
-    await auth.use('api').check()
-    const id = auth.use('api').user!.id
+    const user = await auth.authenticate()
 
-    const record = await Records.create({ userId: id, ...data })
+    const record = await Records.create({ userId: user.id, ...data })
 
     return response.json(record)
   }
 
-  public async show({ auth }: HttpContextContract) {
-    return auth.user
+  public async show({ auth, response }: HttpContextContract) {
+    const user = await auth.authenticate()
+
+    const records = await Records.query().where('user_id', user.id)
+
+    return response.json(records)
   }
 
-  public async update({}: HttpContextContract) {}
+  public async update({ request, response, auth }: HttpContextContract) {
+    const data = await request.validate(UpdateValidator)
 
-  public async destroy({}: HttpContextContract) {}
+    const record = await Records.findByOrFail('record_id', data.recordId)
+
+    const user = await auth.authenticate()
+
+    if (record.userId !== user.id) {
+      return response.status(401).json({
+        error: 'You are not authorized to update this record.'
+      })
+    }
+
+    record.merge(data)
+
+    await record.save()
+
+    return response.send(record)
+  }
+
+  public async destroy({ request, response, auth }: HttpContextContract) {
+    const { recordId } = request.all()
+
+    const record = await Records.findByOrFail('record_id', recordId)
+
+    const user = await auth.authenticate()
+
+    if (record.userId !== user.id) {
+      return response.status(401).json({
+        error: 'You are not authorized to delete this record.'
+      })
+    }
+
+    await record.delete()
+
+    return response.json({ message: 'Record deleted successfully.' })
+  }
 }
